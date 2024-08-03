6b645b3ec86b1c3dc8ae53f4e4f6fd1646403cd0
Nayquo <gregory.naji@proton.me>
2024-08-03 22:35:37 +0200

    commiter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div className="h-screen w-screen flex justify-center items-center">
-          <h1 className="text-4xl">Charles Cantin - Photographerrr</h1>
+          <h1 className="text-4xl">Charles Cantin - Photo</h1>
         </div>
     );
 }
